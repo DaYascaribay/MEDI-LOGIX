db56e07dba2f4368fb210a47a09e0f593bb333b8
DaYascaribay <david-ldu03@hotmail.com>
2025-07-11 19:56:46 -0500

    feat:add elements for project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Aquí iría validación real
+        return redirect(url_for("dashboard"))
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=1019)
